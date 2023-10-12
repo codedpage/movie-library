f308f2454887fd2bffd8dc2d35f509d18db4edf3
manoj kumar <manoj05feb@gmail.com>
2023-10-12 12:00:01 +0530

    restful api added

--- movie/server/main.go
+++ movie/server/main.go
@@ -12,6 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type movieLibraryServer struct {
@@ -19,15 +20,19 @@ type movieLibraryServer struct {
 	movies                                    []*pb.Movie
 }
 
+// u2
 func (s *movieLibraryServer) LoadMovies(ctx context.Context, req *pb.MovieRequest) (*pb.MovieResponse, error) {
 	// Reset the movie library by overwriting the existing movies.
 	s.movies = req.Movies
+
+	fmt.Println(req.Movies)
 
 	return &pb.MovieResponse{
 		StatusCode: http.StatusResetContent,
 	}, nil
 }
 
+// u3
 var movieData = map[string]*pb.Movie{
 	"01-10-2023": {
 		Title:       "Betty-2",
@@ -46,7 +51,7 @@ func (s *movieLibraryServer) GetMovieDetails(ctx context.Context, req *pb.GetMov
 
 	movie, exists := movieData[releaseDate]
 	if !exists {
-		return nil, grpc.Errorf(codes.NotFound, "Movie not found for release date: %s", releaseDate)
+		return nil, status.Errorf(codes.NotFound, "Movie not found for release date: %s", releaseDate)
 	}
 
 	return &pb.GetMovieDetailsResponse{
@@ -54,6 +59,7 @@ func (s *movieLibraryServer) GetMovieDetails(ctx context.Context, req *pb.GetMov
 	}, nil
 }
 
+// u4
 var movieDataMap = map[int32]*pb.Movie{
 	1: {
 		Title:       "Betty-1",
@@ -67,7 +73,7 @@ func (s *movieLibraryServer) UpdateMovieDetails(ctx context.Context, req *pb.Upd
 
 	// Check if the movie exists
 	if _, exists := movieDataMap[movieID]; !exists {
-		return nil, grpc.Errorf(codes.NotFound, "Movie with ID %d not found", movieID)
+		return nil, status.Errorf(codes.NotFound, "Movie with ID %d not found", movieID)
 	}
 
 	// Update the movie details
@@ -80,6 +86,7 @@ func (s *movieLibraryServer) UpdateMovieDetails(ctx context.Context, req *pb.Upd
 	}, nil
 }
 
+// main
 func main() {
 	listen, err := net.Listen("tcp", ":50051")
 	if err != nil {
